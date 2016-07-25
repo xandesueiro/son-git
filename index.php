a538a57d2fb881e1d51dd993b456f5d53786fdd0
ALEXANDRE SUEIRO <lvx.xande@gmail.com>
2016-07-24 21:16:20 -0300

    Adicionando estilo Bootstrap

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
-<meta name="description" content="Estudo Git">
-<meta name="keywords" content="estudo git">
+    <meta charset="utf-8" />
+    <meta name="description" content="Estudo Git">
+    <meta name="keywords" content="estudo git">
+
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <title>Estudos GIT e Github</title>
 </head>
 <body>
-<h1>TREINAMENTO GIT - SCHOOL OF NET</h1>
-<ul>
-<li>As 3 fases</li>
-<li>Servidor Remoto</li>
-<li>Branches</li>
-<li>Merges</li>
-</ul>
+<div class="container">
+    <div class="jumbotron">
+        <h2>TREINAMENTO GIT - SCHOOL OF NET</h2>
+        <ul>
+        <li>As 3 fases</li>
+        <li>Servidor Remoto</li>
+        <li>Branches</li>
+        <li>Merges</li>
+        </ul>
+    </div>
+</div>
 </body>
 </html>
