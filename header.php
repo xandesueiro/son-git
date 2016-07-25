16ab726f98427b0383d73958e3e0f6a5fee06145
ALEXANDRE SUEIRO <lvx.xande@gmail.com>
2016-07-24 21:31:35 -0300

    Ajustando estruturacao do index: header e footer

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Estudo Git">
+    <meta name="keywords" content="estudo git">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <title>Estudos Git e Github</title>
+    
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<meta name="description" content="Estudo Git">
-<meta name="keywords" content="estudo git">
-</head>
-<body>
-<h1>TREINAMENTO GIT - SCHOOL OF NET</h1>
-<ul>
-<li>As 3 fases</li>
-<li>Servidor Remoto</li>
-<li>Branches</li>
-<li>Merges</li>
-</ul>
-</body>
-</html>
+<?php
+include "header.php"
+?>
+
+<div class="container">
+    <div class="jumbotron">
+        <h2>TREINAMENTO GIT - SCHOOL OF NET</h2>
+        <ul>
+        <li>As 3 fases</li>
+        <li>Servidor Remoto</li>
+        <li>Branches</li>
+        <li>Merges</li>
+        </ul>
+    </div>
+</div>
+<?php
+include "footer.php"
+?>
