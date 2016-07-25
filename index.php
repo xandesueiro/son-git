8ad44b9b8d49f4aed743721a933f4da3b6af581b
ALEXANDRE SUEIRO <lvx.xande@gmail.com>
2016-07-24 21:24:25 -0300

    Fatiando Index com Header e Footer

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="description" content="Estudo Git">
-    <meta name="keywords" content="estudo git">
+<?php
+include"header.php";
+?>
 
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <title>Estudos GIT e Github</title>
-</head>
-<body>
 <div class="container">
     <div class="jumbotron">
         <h2>TREINAMENTO GIT - SCHOOL OF NET</h2>
@@ -17,8 +10,11 @@
         <li>Servidor Remoto</li>
         <li>Branches</li>
         <li>Merges</li>
+        <li>Em estudo....</li>
         </ul>
     </div>
 </div>
-</body>
-</html>
+
+<?php
+include"footer.php";
+?>
